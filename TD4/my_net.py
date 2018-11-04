ba222d00cc1e0c4c4bb79d8e05f8033ea0e878c9
Romain Fouilland <romain.fd@gmail.com>
2018-11-04 22:01:32 +0100

    report

--- TD4/my_net.py
+++ TD4/my_net.py
@@ -12,18 +12,69 @@ import tensorflow as tf
 #
 ###############################################################
 
-tf.set_random_seed(2106)
 
 class Network():
+    def __init__(self, neurons_nb=[115], display=False, scaling=False, bagging=True, bag_nb=4, bag_ratio=4, batch_size=100):
+        self.bagging = bagging
+        self.nns = []
+        if bagging:
+            self.bag_nb = bag_nb
+            self.bag_ratio = bag_ratio
+            self.bags = []
+            for i in range(bag_nb):
+                self.nns.append(Bag(neurons_nb=neurons_nb, display=display, scaling=scaling, batch_size=batch_size))
+                self.bags.append([])
+        else:
+            self.nns = [Bag(neurons_nb=neurons_nb, display=display, scaling=scaling, batch_size=batch_size)]
+        self.warm_start = False
 
-    alpha = 0.1
+    def fit(self, X, Y, n_epochs=10):
+        ''' train the network, and if warm_start, then do not reinit. the network
+            (if it has already been initialized)
+        '''
+        if not self.warm_start:
+            # we compute the bags
+            if self.bagging:
+                for i in range(int(self.bag_nb)):
+                    for _ in range(int(X.shape[0] * self.bag_ratio)):
+                        self.bags[i].append(int(X.shape[0] * np.random.random()))  # the index of the item
+            else:
+                self.bags = [list(range(X.shape[0]))]  # we take all the items once
+            self.warm_start = True
 
-    def __init__(self, neurons_nb=[], display=False, scaling=True):
+        for i, nn in enumerate(self.nns):
+            nn.fit(X[self.bags[i]], Y[self.bags[i]], n_epochs=n_epochs)
+        return self
+
+    def predict_proba(self, X):
+        ''' return a matrix P where P[i,j] = P(Y[i,j]=1),
+        for all instances i, and labels j. '''
+        # TODO
+        result = np.zeros((X.shape[0], self.nns[0].neurons_nb[-1]))  # the nb of classes
+        for i, nn in enumerate(self.nns):
+            pred = tf.nn.softmax(nn.logits)  # Apply softmax to logits to have probas
+            if nn.scaling:
+                X = (X - nn.X_mean) / nn.X_std
+            result += nn.sess.run(pred, feed_dict={nn.Xt: X})
+        return result / len(self.nns)
+
+    def predict(self, X):
+        ''' return a matrix of predictions for X '''
+        return (self.predict_proba(X) >= 0.5).astype(int)
+
+    def close(self):
+        for nn in self.nns:
+            nn.sess.close()
+        tf.reset_default_graph()
+
+
+class Bag():
+    def __init__(self, neurons_nb=[130], display=False, scaling=True, batch_size=70):
         ''' initialize the classifier with default (best) parameters '''
         # TODO
         # Parameters
-        self.learning_rate = 0.0005
-        self.batch_size = 150
+        self.learning_rate = 0.0001
+        self.batch_size = batch_size
         self.display_step = 2
         self.display = display
 
@@ -32,12 +83,18 @@ class Network():
         self.neurons_nb = [294]  # nb inputs
         self.weights = {}
         self.biases = {}
-        neurons_nb.append(6)  # nb outputs
+        # neurons_nb.append(6)  # nb outputs
         for i in range(1, 1 + len(neurons_nb)):
             self.neurons_nb.append(neurons_nb[i - 1])
             # Store layers weight & bias
             self.weights["h"+str(i)] = tf.Variable(tf.random_normal([self.neurons_nb[i - 1], self.neurons_nb[i]]))
             self.biases["b"+str(i)] = tf.Variable(tf.random_normal([self.neurons_nb[i]]))
+
+        # output layer
+        self.neurons_nb.append(6)
+        # Store layers weight & bias
+        self.weights["h" + str(1 + len(neurons_nb))] = tf.Variable(tf.random_normal([self.neurons_nb[1 + len(neurons_nb) - 1], self.neurons_nb[1 + len(neurons_nb)]]))
+        self.biases["b" + str(1 + len(neurons_nb))] = tf.Variable(tf.random_normal([self.neurons_nb[1 + len(neurons_nb)]]))
 
         # Data placeholders
         self.Xt = tf.placeholder("float", [None, self.neurons_nb[0]])  # nb inputs
@@ -57,6 +114,11 @@ class Network():
         self.init = tf.global_variables_initializer()
         # starts session
         self.sess = tf.Session()
+        # but not initialized yet
+        self.warm_start = False
+
+        # cost over time
+        self.costs = []
 
     def multilayer_perceptron(self, X):
         previous_layer = X  # we start with the inputs
@@ -67,18 +129,19 @@ class Network():
         out_layer = tf.matmul(previous_layer, self.weights['h' + str(last)]) + self.biases['b' + str(last)]
         return out_layer
 
-    def fit(self, X, Y, warm_start=True, n_epochs=10, scaling=True):
+    def fit(self, X, Y, n_epochs=10):
         ''' train the network, and if warm_start, then do not reinit. the network
             (if it has already been initialized)
         '''
         self.n_data = Y.shape[0]
         # TODO
 
-        if not warm_start:
+        if not self.warm_start:
             self.sess.run(self.init)
             if self.scaling:
                 self.X_mean = np.mean(X, axis=0)
                 self.X_std = np.std(X, axis=0)
+            self.warm_start = True
 
         # auto scale
         if self.scaling:
@@ -99,6 +162,7 @@ class Network():
             # Display logs per epoch step
             if epoch % self.display_step == 0 and self.display:
                 print("Epoch:", '%04d' % (epoch + 1), "cost={:.9f}".format(avg_cost))
+        self.costs.append(avg_cost)
         return self
 
     def predict_proba(self, X):
--- TD4/run_eval.py
+++ TD4/run_eval.py
@@ -1,26 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from my_net import Network
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.multioutput import ClassifierChain
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from time import clock
-from tqdm import tqdm
 
 ###############################################################
 # This is an example script, you may modify it as you wish
 ###############################################################
 
 # Load and parse the data (N instances, D features, L=3 labels)
-XY = np.genfromtxt('./TD4/data/scene.csv', skip_header=1, delimiter=",")
-N, DL = XY.shape
+XY = np.genfromtxt('data/scene.csv', skip_header=1, delimiter=",")
+N,DL = XY.shape
 L = 6
 D = DL - L
-Y = XY[:, 0:L].astype(int)
-X = XY[:, L:D+L]
+Y = XY[:,0:L].astype(int)
+X = XY[:,L:D+L]
 
 # Split into train/test sets
 n = int(N*6/10)
@@ -29,84 +20,20 @@ Y_train = Y[0:n]
 X_test = X[n:]
 Y_test = Y[n:]
 
-# computes the Hamming loss
-def accuracy(clsf, X_test, Y_test):
-    Y_pred = clsf.predict(X_test)
-    loss = np.mean(Y_pred != Y_test)
-    return loss
+from time import clock
+t0 = clock()
 
-def testClassifier(clsf, X_train, Y_train, X_test, Y_test, maxTime=3, NbEpochs=10, homeMade=False):
-    t0 = clock()
-    # the accuracy over the training
-    accTest, accTrain, bestAcc = [], [], 1
+# Test our classifier 
+h = Network()
 
-    if homeMade:
-        clsf.fit(X_train, Y_train, warm_start=False)
-        accTest.append(accuracy(clsf, X_test, Y_test))
-        accTrain.append(accuracy(clsf, X_train, Y_train))
-        i = 0
-        while (clock() - t0) < maxTime:
-            clsf.fit(X_train, Y_train, n_epochs=NbEpochs)
-            testAcc = accuracy(clsf, X_test, Y_test)
-            if testAcc < bestAcc:
-                bestAcc = testAcc
-            accTest.append(testAcc)
-            accTrain.append(accuracy(clsf, X_train, Y_train))
-            i = i + NbEpochs
-        print("Trained %d epochs in %d seconds." % (i, int(clock() - t0)))
-        plt.plot(range(1, i // NbEpochs + 2), accTest, label='Test')
-        plt.plot(range(1, i // NbEpochs + 2), accTrain, label='Train')
-        plt.xlabel("Number of epochs (x10)")
-        plt.ylabel("Hamming loss")
-        plt.title("Hamming loss vs. number of epochs")
-        plt.legend()
-        plt.show()
-    else:
-        clsf.fit(X_train, Y_train)
-        print("Trained in %d milliseconds." % (int(1000*(clock() - t0))))
+i = 0
+while (clock() - t0) < 1:
+    h.fit(X_train, Y_train, n_epochs=10)
+    i = i + 10
 
-    print("Hamming loss: {:.2%}, best loss {:.2%}".format(accuracy(clsf, X_test, Y_test), bestAcc))
-    return bestAcc
-
-# Our NN network
-def testNbOfNeurons(i0, i1, step, maxTime):
-    bestAccs = []
-    for neurons_nb in tqdm(range(i0, i1, step)):
-        h = Network(neurons_nb=[neurons_nb], display=False, scaling=False)
-        acc = testClassifier(h, X_train, Y_train, X_test, Y_test, maxTime=maxTime, NbEpochs=10, homeMade=True)
-        bestAccs.append(acc)
-        h.sess.close()
-    plt.plot(range(i0, i1, step), bestAccs)
-    plt.show()
-# testNbOfNeurons(10, 50, 5, 120)  # => best at 35
-
-h = Network(neurons_nb=[35], display=False, scaling=False)
-acc = testClassifier(h, X_train, Y_train, X_test, Y_test, maxTime=15, NbEpochs=10, homeMade=True)
-h.sess.close()
-
-def benchmarks():
-    # sklearn.multiclass.OneVsRestClassifier
-    print("------------------- OneVsRestClassifier (Naive Bayes) -------------------")
-    oneVsRest = OneVsRestClassifier(MultinomialNB())
-    testClassifier(oneVsRest, X_train, Y_train, X_test, Y_test)
-    print("--------------- OneVsRestClassifier (Logistic Regression) ---------------")
-    base_lr = LogisticRegression(solver='lbfgs')
-    oneVsRest = OneVsRestClassifier(base_lr)
-    testClassifier(oneVsRest, X_train, Y_train, X_test, Y_test)
-
-    # sklearn.multiouput.ClassifierChain
-    print("---------------------------- ClassifierChain ----------------------------")
-    clsfChain = ClassifierChain(base_lr)
-    testClassifier(clsfChain, X_train, Y_train, X_test, Y_test)
-
-    # sklearn.neighbors.KNeighborsClassifier
-    print("-------------------------- KNeighborsClassifier -------------------------")
-    neigh = KNeighborsClassifier()
-    testClassifier(neigh, X_train, Y_train, X_test, Y_test)
-
-    # sklearn.tree.DecisionTreeClassifier
-    print("------------------------- DecisionTreeClassfier -------------------------")
-    neigh = DecisionTreeClassifier()
-    testClassifier(neigh, X_train, Y_train, X_test, Y_test)
-
-# benchmarks()
+print("Trained %d epochs in %d seconds." % (i, int(clock() - t0)))
+Y_pred = h.predict(X_test)
+print(Y_test)
+print(Y_pred)
+loss = np.mean(Y_pred != Y_test)
+print("Hamming loss: ", loss)
